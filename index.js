0985e78bbee47bcad8c35ea32ad5b0e5b47a2d10
Omar Nehad <omarnehad@icloud.com>
2026-01-25 22:27:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const {
-  Client,
-  GatewayIntentBits,
-  Partials,
-  Routes,
-  ActionRowBuilder,
-  StringSelectMenuBuilder,
-  ModalBuilder,
-  TextInputBuilder,
-  TextInputStyle,
-  EmbedBuilder
-} = require("discord.js");
+const {Client,GatewayIntentBits,Rest,Partials,Routes,ActionRowBuilder,StringSelectMenuBuilder,ModalBuilder,TextInputBuilder,TextInputStyle,EmbedBuilder} = require("discord.js");
 
 const { REST } = require("@discordjs/rest");
 const fs = require("fs");
